fe0fa09c9f85204323594fc2aaa13bc98c4bca48
WuPX <1157720295@qq.com>
2019-07-24 09:19:44 +0800

    test

--- b.py
+++ b.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
 browser = webdriver.Chrome()
 browser.get('http://localhost:8001')
-assert 'Django' in browser.title
+assert 'To-do' in browser.title,"Browse.title was" + browser.title
